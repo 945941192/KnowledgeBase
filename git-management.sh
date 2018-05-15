9cceefbef167bbca3d9490ba6092cd043d7b4a13
weizhanbiao <weizhanbiao@ali-186590d97253.local>
2018-05-15 09:36:22 +0800

    add something

--- git-management.sh
+++ git-management.sh
@@ -3,5 +3,9 @@
 LOGDATE=$(date "+%Y-%m-%d-%H-%M-%S")
 GitProjectDir=$(pwd)
 GitLogFile="${GitProjectDir}/git_log"
-cd ${GitProjectDir} && git status >> ${GitLogFile} && git add -A && git commit -m "add something"
+
+cd ${GitProjectDir} 
+res=$(git status | grep "modified" | wc -l)  
+[ ${res} -eq 0 ] && echo "${LOGDATE}------> Nothing need to commit" && exit 88
+git status >> ${GitLogFile} && git add -A && git commit -m "add something"
 [ $? -eq 0 ] && echo "${LOGDATE}------->commit ok" >> ${GitLogFile} 
