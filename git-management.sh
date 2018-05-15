10c9eecfe3feff873bf69fa1028914bc2f8b13b2
weizhanbiao <weizhanbiao@ali-186590d97253.local>
2018-05-15 10:55:01 +0800

    add something

--- git-management.sh
+++ git-management.sh
@@ -7,6 +7,6 @@ GitLogFile="${GitProjectDir}/git_log"
 
 cd ${GitProjectDir} 
 res=$(git status | grep "modified" | wc -l)  
-[ ${res} -eq 0 ] && echo "${LOGDATE}------> Nothing need to commit" | tee -a ${GitLogFile} && exit 88
+[ ${res} -eq 1 ] && echo "${LOGDATE}------> Nothing need to commit" | tee -a ${GitLogFile} && exit 88
 git status >> ${GitLogFile} && git add -A && git commit -m "add something" && git push origin wzb:wzb
 [ $? -eq 0 ] && echo "${LOGDATE}------->commit ok" | tee -a ${GitLogFile}
